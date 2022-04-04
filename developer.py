56ce557b15c5302d6156956b1593af482f4f121e
Muzamal Sheikh <muzamal2012@gmail.com>
2022-04-04 03:59:31 -0400

    v29

--- developer.py
+++ developer.py
@@ -1,13 +1,13 @@
 developer = {
     "name": "Muzamal Sheikh",
     "aspiration": "Software Developer / Software Engineer/ Full-Stack Developer",
-    "Major": "Computer Science",
-    "Masters": "Data Science",
-    "Minor": "Mathematics",
+    "major": "Computer Science",
+    "masters": "Data Science",
+    "minor": "Mathematics",
     "college": "Ramapo College of New Jersey",
     "languages": ["Python", "C++" ,"JavaScript" ,"C#", "TypeScript", "Kotlin"],
     "webDevelopment": ["React", "Node"],
     "database": ["ProgreSQL", "MongoDB"],
     "technologies": ["VS Code"]
-} 
+}   
 
